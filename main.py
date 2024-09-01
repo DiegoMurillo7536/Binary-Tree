1209bf13d211d9654e51c2616df4ee2a1e0bfaf8
Diego Murillo <diegoa31052004@gmail.com>
2024-09-01 18:07:24 -0500

    [ADD] Sacha and Karina exercises

--- main.py
+++ main.py
@@ -13,11 +13,20 @@ def main():
     # Revisar aquellos nodos que al menos tengan dos hijos
     arbol.recorrer_nodos_con_dos_hijos()
 
-    #Revisar aquellos nodos que tengan al menos un hijo
-    arbol.recorrer_nodos_almenos_con_un_hijo()
+    # Revisar aquellos nodos que tengan al menos un hijo
+    arbol.recorrer_nodos_almenos_con_un_hijo_par()
+
+    # Suma de cada nodo
+    arbol.visualizar_suma_hijos()
     
-    #Recorrer camino para buscar un número
+    # Recorrer camino para buscar un número
     valor_a_encontrar = insert_number_by_user_to_search()
-    
     arbol.encontrar_camino(valor_a_encontrar)
+
+
 main()
+
+"""Output:
+Ingresa un número que desees buscar:7
+El valor 10 fue encontrado y este fue el camino antes de encontrar el valor buscado [8, 6, 7]
+"""
